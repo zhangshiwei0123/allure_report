d7351637a55fb20ced74c9e4440246f23cf9e5de
zhangshiwei0123 <15135309421_zsw@sina.com>
2021-03-11 13:00:00 +0800

    提交allure的代码

---
+++ scripts/test_hello.py
@@ -0,0 +1,11 @@
+class TestHello:
+
+    def test_case1(self):
+        print("这是第一条case")
+
+    def test_case2(self):
+        print("这是第二条case")
+
+    def test_case3(self):
+        print("这是第三条case")
+
